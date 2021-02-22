76d7ed8d28fef751f5a7cc48dc56497eeb9110ac
nerajno <nerajno@gmail.com>
2021-02-22 16:54:12 -0500

    completed styling

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ### Todos
 
-- [] Create a good ReadMe
-- [] Hide api keys
+- [ ] Create a good ReadMe
+- [ ] Hide api keys
+- [ ] Hide api~addresses/ paths
 
 Api key : https://api.themoviedb.org/3/movie/550?api_key=cc9531648a8a47fec779d75e4d9461f6
 featured: https://api.themoviedb.org/3/discover/movie?api_key=cc9531648a8a47fec779d75e4d9461f6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1
--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import Movie from "./components/Movie";
 const FEATURED_API =
   "https://api.themoviedb.org/3/discover/movie?api_key=cc9531648a8a47fec779d75e4d9461f6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
 
-const SEARCH_API =
-  "https://api.themoviedb.org/3/search/movie?api_key=cc9531648a8a47fec779d75e4d9461f6&language=en-US&page=1&include_adult=false";
+const SEARCH_API="https://api.themoviedb.org/3/search/movie?&api_key=a607d8d4f02314295a958f2286e64a46&query=";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState(" "); 
+  const [searchTerm, setSearchTerm] = useState(""); 
+
+  // const getMovies = (API) =>{
+  //   fetch(FEATURED_API)
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     // console.log(data);
+  //     setMovies(data.results);
+  //   });
+  // }
 
   useEffect(() => {
     fetch(FEATURED_API)
@@ -22,13 +30,15 @@ function App() {
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
+    if(searchTerm){
     fetch(SEARCH_API+searchTerm)
       .then((res) => res.json())
       .then((data) => {
          console.log(data);
         setMovies(data.results);
       });
+      setSearchTerm('');
+    }
   };
 
   const handleOnChange = (e) => {
