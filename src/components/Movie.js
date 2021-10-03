10faab32d9a5b2f1886f0b271d2f94de75847a22
nerajno <nerajno@gmail.com>
2021-10-02 20:15:54 -0400

    still trying to fix hover features

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,30 +1,36 @@
-import React from 'react'; 
+import React from "react";
 
+const IMAGE_API = "https://image.tmdb.org/t/p/w1280";
 
-
-const setVoteClass=(vote)=>{
-    if(vote >=7){
-        return "green"
-    }else if (vote >= 5){
-        return "orange"
+const Movie = ({ title, poster_path, overview, vote_average }) => {
+  const setVoteClass = (vote) => {
+    if (vote >= 8) {
+      return "green";
+    } else if (vote >= 6) {
+      return "orange";
     } else {
-        return "red";
+      return "red";
     }
-}
+  };
 
-const Movie = ({title, poster_path, overview, vote_average}) => (
+  return (
     <div className="movie">
-        {/* TODO: Add wikipedia search link here, open in newtab. */}
-        <img src={poster_path ? process.env.REACT_APP_IMG_API+poster_path : "https://images.pexels.com/photos/3945317/pexels-photo-3945317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"} alt={title} />
-        <div className="movie-info">
-            <h3>{title}</h3>
-            <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
-        </div>
-        <div className="movie-overview">
-            <h2>Overview: </h2>
-            <p>{overview}</p>
-        </div>
-    </div>
-);
+      {poster_path ? (
+        <img src={IMAGE_API + poster_path} alt={title} />
+      ) : (
+        <img src="no-cover.png" alt={title} />
+      )}
+      <div className="movie-info">
+        <h3>{title}</h3>
+        <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
+      </div>
 
-export default Movie; 
+      <div className="movie-overview">
+        <h2>Overview:</h2>
+        <p>{overview}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Movie;
