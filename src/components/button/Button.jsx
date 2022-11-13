56201cfc0e7027248a5b66d89df83c06bdfed279
Nerando Johnson <nerajno@gmail.com>
2022-11-13 09:56:33 -0500

    completed plan

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ProTypes from 'prop-types';
+
+const Button = props => {
+    return (
+        <button 
+        className={`btn ${props.className}`}
+        onClick={props.onClick ? () => props.onClick() : null}
+        >
+            {ProTypes.children}
+        </button>
+    )
+}
+
+const  OutlineButton = props => {
+    return (
+        <Button
+        className={`btn-outline ${props.className}`}
+        onClick={props.onClick ? () => props.onClick() : null}
+        >
+        {props.children}
+        </Button>
+    )
+}
+
+
+Button.propTypes = {
+    onClick: ProTypes.func
+}
+
+export default Button;
+
