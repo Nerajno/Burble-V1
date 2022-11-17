bdc699f3f69115245e8fa2672aba1774a14a8552
Nerando Johnson <nerajno@gmail.com>
2022-11-17 09:07:13 -0500

    Day 11

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -2,8 +2,8 @@ const apiConfig ={
     baseUrl: 'https://api.themoviedb.org/3/',
     apiKey: 'd5199020aac77d6859c71466db7228e1', // TODO: need  hide this key
     //originalImage:'https://image.tmdb.org/t/p/original/${imgPath}',
-    originalImage: (imgPath) => 'https://image.tmdb.org/t/p/original/${imgPath}',
-    wq500Image: (imgPath) => 'https://image.tmdb.org/t/p/w500/${imgPath}',
+    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+    wq500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 }
 
  console.log(apiConfig.originalImage);
