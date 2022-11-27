bbf0370b111eabb8dfca523804b87ebd1ff0a8a1
Nerando Johnson <nerajno@gmail.com>
2022-11-27 08:53:08 -0500

    Implemented Page error

---
+++ src/components/page-header/PageHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './page-header.scss'
+import bg from '../../assets/footer-bg.jpg';
+
+
+
+const PageHeader = props => {
+        return (
+            <div>
+
+            </div>
+        )
+}
+
+export default PageHeader
--- src/components/pages/Catalog.jsx
+++ src/components/pages/Catalog.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useParams } from 'react-router';
-
+import PageHeader from '../page-header/PageHeader';
+import { catergory as cate } from '../api/tmdbApi';
 
 const Catalog = () => {
     const {category}  = useParams();
     console.log(category);
     return (
-        <div>
-            Catalog
-        </div>
+        <>
+        <PageHeader>
+            {category == cate.movie ? 'Movies' : 'TV Series'  }
+        </PageHeader>
+        </>
     );
 }
 
