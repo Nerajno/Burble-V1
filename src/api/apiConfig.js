df453b6366e1747d6ba587eb4c21789209984642
Nerando Johnson <nerajno@gmail.com>
2022-12-06 08:37:19 -0500

    still stuck

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,13 +1,13 @@
 const apiConfig ={
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'd5199020aac77d6859c71466db7228e',
-    //apiKey: process.env.REACT_APP_API_KEY, // TODO: need  hide this key
+    // apiKey: process.env.REACT_APP_API_KEY,
+    apiKey: 'd5199020aac77d6859c71466db7228e1',
     //originalImage:'https://image.tmdb.org/t/p/original/${imgPath}',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 }
 
-//  console.log(apiConfig.originalImage);
+console.log(process.env.REACT_APP_API_KEY, "butter");
 
 // console.log("https://api.themoviedb.org/3/movie/550?api_key=d5199020aac77d6859c71466db7228e1");
 
