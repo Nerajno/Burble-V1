4c102ed8db3c0de852cc08b7677591bc5148bc50
Nerando Johnson <nerajno@gmail.com>
2022-12-23 07:10:03 -0500

    reverting to last previous working state

--- src/contexts/ThemeContext.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const initialThemeState = {
-    theme: "light",
-    setTheme: () => null
-};
-
-const ThemeContext = React.createContext(initialThemeState);
-export default  ThemeContext;
