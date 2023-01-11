ae66aace7c6e4f7c45047d46a7496f37fa60d724
Nerando Johnson <nerajno@gmail.com>
2023-01-10 21:15:34 -0500

    Started adding cypress files

---
+++ cypress/e2e/navTest.cy.js
@@ -0,0 +1,7 @@
+/* ==== Generated with Cypress Studio ==== */
+cy.visit('http://localhost:2000/');
+cy.get('.header__nav > :nth-child(2) > a').click();
+cy.get('.header__nav > :nth-child(3) > a').click();
+cy.get('.header__nav > :nth-child(1) > a').click();
+cy.get('.header__nav > :nth-child(2) > a').click();
+/* ==== End Cypress Studio ==== */
