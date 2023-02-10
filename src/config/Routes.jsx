08270ec11490f36c972e21c01bfca2bb81cd1abd
Nerando Johnson <nerajno@gmail.com>
2023-02-09 20:20:22 -0500

    404 implementation

--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -5,14 +5,16 @@ import {Route, Switch} from "react-router-dom";
 import Home from "../components/pages/Home";
 import Catalog from "../components/pages/Catalog";
 import Detail from "../components/pages/detail/Detail";
+import NotFound from "../components/pages/NotFoundPage";
 
 const Routes = () => {
 	return (
 		<Switch>
-			<Route path="/:category/search/:keyword" component={Catalog} />
-			<Route path="/:category/:id" component={Detail} />
-			<Route path="/:category" component={Catalog} />
+			<Route path="/:category/search/:keyword" exact component={Catalog} />
+			<Route path="/:category/:id" exact component={Detail} />
+			<Route path="/:category" exact component={Catalog} />
 			<Route path="/" exact component={Home} />
+			<Route path="*" component={NotFound} />
 		</Switch>
 	);
 };
