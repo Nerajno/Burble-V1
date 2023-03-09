6f70c5b980b736821c9c99abe09dce27a67113cc
Nerando Johnson <nerajno@gmail.com>
2023-03-08 21:17:49 -0500

    commit for commiting sake

--- src/components/header/Header.cy.jsx
+++ src/components/header/Header.cy.jsx
@@ -1,20 +1,6 @@
 import React from "react";
 import Header from "./Header";
 
-describe("<Header />", () => {
-//   it("should render the header", () => {
-//     cy.mount(<Header />);
-//     cy.get('header').should('not.have.class', 'header');
-//   });
-  // it('should add the "shrink" class to the header on scroll', () => {
-  //   cy.get('.header').should('not.have.class', 'shrink');
-  //   cy.get('body').scrollTo('top');
-  //   cy.get('.header').should('have.class', 'shrink');
-  // });
-// });
-
-});
-
 // Test conditions
 // Header
 // - has all expected links.
@@ -22,50 +8,9 @@ describe("<Header />", () => {
 // - should have a logo image with alt text.
 
 
-describe('Header Component', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
-
-  it('displays all expected links', () => {
-    const headerNav = [
-      { 
-          display: 'Home',
-          path: '/'
-      },
-      {
-          display: 'Movies',
-          path: '/movie'
-      }, 
-      {
-          display: 'TV Series',
-          path: '/tv'
-      }
-    ];
-
-    cy.get('.header__nav')
-      .find('li')
-      .each(($el, index) => {
-        cy.wrap($el)
-          .should('contain', headerNav[index].display)
-          .find('a')
-          .should('have.attr', 'href', headerNav[index].path);
-      });
-  });
-
-  it('links are real links with <a> tags', () => {
-    cy.get('.header__nav')
-      .find('li')
-      .each(($el) => {
-        cy.wrap($el)
-          .find('a')
-          .should('have.prop', 'tagName', 'A');
-      });
-  });
-
-  it('has a logo image with alt text', () => {
-    cy.get('.logo')
-      .find('img')
-      .should('have.attr', 'alt');
+describe('<Header />', () => {
+  it('renders', () => {
+    cy.mount(<Header></Header>);
+    cy.get('header').should('have.class', 'header');
   });
 });
