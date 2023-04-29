04dc8eed1069aa090ebafbe0d16f919e1543a424
Nerando Johnson <nerajno@gmail.com>
2023-04-29 12:38:02 -0400

    Review for v2 implementation

--- src/components/cypress.config.js
+++ src/components/cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
 		setupNodeEvents(on, config) {
 			return require("./cypress/plugins/index.js")(on, config);
 		},
-		baseUrl: "http://localhost:2000",
+		// baseUrl: "http://localhost:2000",
 	},
 
   component: {
