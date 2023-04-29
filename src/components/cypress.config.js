459c71cc369ea20f9bc76ea9ca19b62398965032
Nerando Johnson <nerajno@gmail.com>
2023-04-29 11:16:23 -0400

    Reinstalled cypress on the new machine and configuring it accordingly

--- src/components/cypress.config.js
+++ src/components/cypress.config.js
@@ -1,13 +1,16 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+	projectId: "y47h8y",
+	e2e: {
+		experimentalStudio: true,
+		setupNodeEvents(on, config) {
+			return require("./cypress/plugins/index.js")(on, config);
+		},
+		baseUrl: "http://localhost:2000",
+	},
 
-  "component": {
+  component: {
     "devServer": {
       "framework": "create-react-app",
       "bundler": "webpack"
